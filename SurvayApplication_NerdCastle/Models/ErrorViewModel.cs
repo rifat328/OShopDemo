34c0eb3717ebd2705b0e54b6f24c9e47e5d4d2f0
Rifat <rifathossain328663@gmail.com>
2023-09-15 19:57:34 +0600

    Initial Change 0 : SurvayApplication Challenge 3

---
+++ SurvayApplication_NerdCastle/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SurvayApplication_NerdCastle.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
